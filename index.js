339c28ce245dbd1f3febe63fa4485d36f58ae966
Cruz Rocha <juancruzrocha@live.com>
2023-03-01 21:05:36 -0300

    Revert "Cors intalled"
    This reverts commit 6087d2b46d3aefb42d22f9f369153ce176886205.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const { completeApiModified } = require('./controller');
 const app = express();
-const cors = require('cors')//CRUZ
-
-server.use(cors()) // que venga cualquier cliente a querer comunicarse con mi servidor (CRUZ)
 
 app.get('/products', async (req, res) => {
    return res.send(await completeApiModified());
