f8817c1c241f5b79731fadb76a75853199eb58bc
Cruz Rocha <juancruzrocha@live.com>
2023-03-01 22:27:46 -0300

    trying cors again
    intentando intalar el cors pero esta vez con el comando npm intall cors --save

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const { completeApiModified } = require('./controller');
 const app = express();
+const cors = require('cors');
+
+
+app.use(cors());
 
 app.get('/products', async (req, res) => {
    return res.send(await completeApiModified());
